2de0de94f3ddfdb1e1c70b14b7bd3ca6330b26fd
ozzasci <ozzasci@gmail.com>
2025-06-13 12:58:21 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('kelime-bildirim-v1').then(cache => {
+            return cache.addAll([
+                '/index.html',
+                'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
+                'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
