d0d5417355d003450a04394b09b826fb494bdcdd
ozzasci <ozzasci@gmail.com>
2025-06-18 10:51:10 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('kelime-bildirim-v1').then(cache => {
-            return cache.addAll([
-                '/index.html',
-                'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
-                'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
-            ]).catch(err => {
-                console.error('Önbellekleme hatası:', err);
-            });
-        })
-    );
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open('kelimelerim-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/script.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
 });
-
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request).catch(err => {
-                console.error('Ağ isteği hatası:', err);
-                return caches.match('/index.html');
-            });
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
 });
