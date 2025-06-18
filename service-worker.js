263c1494f2e9e342e64f55d78a272ffb04d58793
ozzasci <ozzasci@gmail.com>
2025-06-18 12:26:13 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,12 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('kelimelerim-v1').then(cache => {
       return cache.addAll([
-        './',
-        './index.html',
-        './script.js',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
+        '/',
+        '/index.html',
+        '/script.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
       ]);
     })
   );
